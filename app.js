66369663efb000cb5ab7eb4bb8e435e8b1d7542b
juligarc91 <julissagarcia@pursuit.org>
2024-02-23 17:04:22 -0500

    added get route and 404 page

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const express = require('express');
 //config
 const app = express();
 
+// ROUTES
+app.get('/', (req, res) => {
+    res.status(200).json({ "message" : "Hello, World!" })
+});
+
+// 404 page
+app.get('*', (req, res) => {
+    res.status(404).json({ error: 'Sorry, page not found' })
+});
+
 //export
 module.exports = app;
