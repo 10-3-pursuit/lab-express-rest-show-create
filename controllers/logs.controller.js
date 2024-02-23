0f25f7c5ee649d9ac1d605c03738f8f459113469
juligarc91 <julissagarcia@pursuit.org>
2024-02-23 18:39:25 -0500

    added path for log id

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -6,4 +6,10 @@ logs.get('/', (req, res) => {
     res.status(200).json({ logs: logsData });
 });
 
+logs.get('/:id',(req, res) =>{
+    const { id } = req.params
+    const selectedLog = logsData.find(log => log.id === +id)
+    selectedLog ? res.status(200).json(selectedLog) : res.status(400).json({ message: `Log with ID ${id} could not be found.` })
+});
+
 module.exports = logs;
