4d30232044f4a5e3ca32a565e577b806466494cb
juligarc91 <julissagarcia@pursuit.org>
2024-02-23 18:10:07 -0500

    mounted controller to path on app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.status(200).json({ "message" : "Hello, World! :-]" })
 });
 
+// logs
+const logsController = require('./controllers/logs.controller');
+app.use('/logs', logsController);
+
 // 404 page
 app.get('*', (req, res) => {
     res.status(404).json({ error: 'Sorry, page not found' })
