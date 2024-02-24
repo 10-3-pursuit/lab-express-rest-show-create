2db544b5f936c212524e76449a297fff53fd8340
AnitaOwen <anitaowen@pursuit.org>
2024-02-24 17:10:00 -0500

    fixed newID error

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -15,7 +15,7 @@ logs.get("/", (req, res) => {
   })
 
   logs.post('/', (req, res) => {
-    const newID = logsArray[logsArray.length - 1] +1
+    const newID = logsArray[logsArray.length - 1].id +1
     req.body.id = newID
     logsArray.push(req.body)
     res.json({ logs: logsArray })
