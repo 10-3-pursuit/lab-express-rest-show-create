e6ab4628cbcede70bf2b332ef31ca70a2ec09dd5
enochtagoe <enochtagoe@pursuit.org>
2024-02-27 15:53:02 -0500

    save updates and changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // DEPENDENCIES
 const express = require('express')
+const cors = require('cors');
 
 // CONFIGURATION
 const app = express();
+app.use(cors());
 
 //MIDDLEWARE FOR FETCH RQUESTS other than GET
 //the reason why you need this is that incoming request are in html etc; this modifies the data so it can be put in json
@@ -10,7 +12,7 @@ app.use(express.json());
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('HELLO WORLD')
+  res.send('HELLO WORLD!!!')
 })
 //routing logs from controller and data
 //you need to "require"  a data by by creating a  CONSTANT variable and by using CONST
--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const logs = express.Router();
 
-const logsData = require('../models/logs.model')
+const logsData = require('../models/logs.model');
 
 logs.get('/', (req, res)=> {
     res.json({logs: logsData})
